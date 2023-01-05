b34016d3d3669aa3edf873d9bb9c58d38680bd9d
Nabarun <nicknabarun@gmail.com>
2023-01-05 22:16:32 +0530

    Initial commit

---
+++ components/index.js
@@ -0,0 +1,5 @@
+export { default as Navbar } from "./Navbar";
+export { default as Layout } from "./Layout";
+export { default as ThemeSwitcher } from "./ThemeSwitcher";
+export { default as Dropdown } from "./Dropdown";
+export { default as Footer } from "./Footer";
