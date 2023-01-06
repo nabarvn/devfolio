45d2572370c1f0e93981679187681b30e3a5a05c
Nabarun <nicknabarun@gmail.com>
2023-01-06 18:30:09 +0530

    Initial commit

---
+++ components/Topbar.jsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import { Dropdown } from ".";
+import ThemeSwitcher from "./ThemeSwitcher";
+
+const Topbar = () => {
+  return (
+    <div className='container mx-auto px-9 mb-8'>
+      <div className='flex justify-between border-b w-full py-3 px-1'>
+        <div className='self-center float-start'>
+          <Link href='/'>
+            <span className='cursor-pointer font-bold text-2xl dark:text-night-white'>
+              "gm"
+            </span>
+          </Link>
+        </div>
+
+        <div className='flex float-end'>
+          {/* <div className='hidden md:contents'>
+            <Link href='/about' className='self-center mx-2'>
+              <span className='cursor-pointer font-bold text-lg dark:text-night-white'>
+                "about"
+              </span>
+            </Link>
+            <Link
+              href='https://www.blog.nabarun.xyz'
+              className='self-center mx-2'
+            >
+              <span className='cursor-pointer font-bold text-lg dark:text-night-white'>
+                "blog"
+              </span>
+            </Link>
+            <Link href='/projects' className='self-center mx-2'>
+              <span className='cursor-pointer font-bold text-lg dark:text-night-white'>
+                "projects"
+              </span>
+            </Link>
+          </div> */}
+          <div className='self-center hidden md:contents'>
+            <ThemeSwitcher />
+          </div>
+          <div className='self-center ml-2 md:hidden'>
+            <Dropdown />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
