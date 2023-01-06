55ad69bf2bf9a8c5ba429aff1b3e1351aaa504d2
Nabarun <nicknabarun@gmail.com>
2023-01-06 18:29:26 +0530

    Minor change

--- components/index.js
+++ components/index.js
@@ -1,4 +1,4 @@
-export { default as Navbar } from "./Navbar";
+export { default as Topbar } from "./Topbar";
 export { default as Layout } from "./Layout";
 export { default as ThemeSwitcher } from "./ThemeSwitcher";
 export { default as Dropdown } from "./Dropdown";
