9e39d10f2e7dfa2e3ad8b5f6cf3f825aaa7cd43d
Nabarun <nicknabarun@gmail.com>
2023-01-20 17:58:55 +0530

    Change grid design

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,82 +1,67 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import { MdOutlineWavingHand } from "react-icons/md";
+import { Navbar } from "../components";
 
 const Home = () => {
   return (
-    <div className='container mx-auto px-10'>
+    <div
+      id='layout-child'
+      className='container mx-auto mb-auto px-10 min-h-screen'
+    >
       <Head>
         <title>Nabarun</title>
         <link rel='icon' href='/nabarun.png' />
       </Head>
-      {/* <div className='flex flex-col md:p-9 md:pb-0'>
-        <Image
-          alt='Nabarun.eth'
-          unoptimized
-          height={100}
-          width={100}
-          className='align-middle h-20 w-20 md:h-32 md:w-32 float-left rounded-full'
-          src='/nabarun.png'
-        />
-
-        <div className='md:max-w-4xl mt-5 md:mt-9'>
-          <h1 className='text-2xl md:text-5xl font-bold'>
-            Software developer, web3 degen, and an amateur guitarist.
-          </h1>
-          <p className='text-base md:text-justify font-bold mt-5 md:mt-9'>
-            I'm Nabarun, a software developer and web3 autodidact based in India
-            currently. Along with taking keen interest in this space, I truly
-            believe that this is the next frontier and everyone should be
-            comfortable with it. The programmable blockchain tech has already
-            made huge dents out there and will continue doing so in all the
-            industries we can possibly imagine rn. And it all starts with NFTs.
-            I'm well aware of the fact that we're still early and there's a ton
-            for me to learn both theoretically and practically at work, which
-            makes me even more excited for the future opportunities where I can
-            find my niche.
-          </p>
-        </div>
-      </div> */}
-
-      <div className='grid grid-cols-1 md:grid-cols-12 gap-2'>
-        <div className='col-span-1 md:col-span-11'>
-          <div className='flex flex-col md:p-9 md:pb-0'>
+      <div className='grid grid-cols-1 md:grid-cols-12'>
+        <div className='col-span-1 md:col-span-10 lg:col-span-11'>
+          <div className='flex flex-col md:px-9'>
             <Image
               alt='Nabarun.eth'
               unoptimized
               height={100}
               width={100}
-              className='align-middle h-20 w-20 md:h-32 md:w-32 float-left rounded-full'
+              className='align-middle h-20 w-20 md:h-28 md:w-28 float-left rounded-full'
               src='/nabarun.png'
             />
 
-            <div className='md:max-w-4xl mt-5 md:mt-9'>
-              <h1 className='text-2xl md:text-5xl font-bold'>
+            <div id='layout-child' className='md:max-w-4xl mt-5 md:mt-7'>
+              <h1 className='text-3xl md:text-5xl font-bold cursor-default text-teal-700 dark:text-night-teal'>
                 Software developer, web3 degen, and an amateur guitarist.
               </h1>
-              <p className='text-base md:text-justify font-bold mt-5 md:mt-9'>
-                <span className='flex mb-2'>
-                  <span>Hi</span>
+              <div className='text-lg text-justify font-semibold cursor-default mt-5 md:mt-9'>
+                <span className='flex'>
+                  <span className='text-lg md:text-2xl italic text-cyan-700 dark:text-purple-300'>
+                    Hi fren
+                  </span>
                   <span className='self-center'>
-                    <MdOutlineWavingHand className='h-5 w-5 ml-1 text-yellow-600' />
+                    <MdOutlineWavingHand className='h-5 w-5 md:h-7 md:w-7 ml-2 text-yellow-500 dark:text-yellow-300' />
                   </span>
                 </span>
-                I'm Nabarun, a software developer and web3 autodidact based in
-                India currently. Along with taking keen interest in this space,
-                I truly believe that this is the next frontier and everyone
-                should be comfortable with it. The programmable blockchain tech
-                has already made huge dents out there and will continue doing so
-                in all the industries we can possibly imagine rn. And it all
-                starts with NFTs. I'm well aware of the fact that we're still
-                early and there's a ton for me to learn both theoretically and
-                practically at work, which makes me even more excited for the
-                future opportunities where I can find my niche.
-              </p>
+                <span className='flex text-lg md:text-2xl italic text-cyan-700 dark:text-purple-300 mb-5'>
+                  I'm Nabarun
+                </span>
+                <p className='text-night-gray dark:text-gray-300 mb-5'>
+                  A software developer and web3 autodidact based in India
+                  currently. Along with taking keen interest in this space, I
+                  truly believe that this is the next frontier and everyone
+                  should be comfortable with it. Programmable blockchain tech
+                  has already made huge dents out there and will continue doing
+                  so in all the industries we can possibly imagine rn. And it
+                  all starts with NFTs.
+                </p>
+                <Link href='/email'>
+                  <button className='transition duration-300 transform bg-blue-500 dark:bg-night-blue hover:bg-blue-900 dark:hover:bg-indigo-700 inline-block text-lg font-medium rounded-full text-white px-3 py-1 cursor-pointer'>
+                    Email Me!
+                  </button>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-        <div className='col-span-1 md:col-span-1 hidden md:contents'>
-          <div className='relative md:sticky top-8'>About</div>
+        <div className='col-span-1 md:col-span-2 ld:col-span-1 hidden md:contents'>
+          <Navbar />
         </div>
       </div>
     </div>
