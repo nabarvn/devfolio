c6c262af69fcef3a28a907caf56dec9e05cac5f6
Nabarun <nicknabarun@gmail.com>
2023-01-20 18:01:16 +0530

    Add new components

--- components/index.js
+++ components/index.js
@@ -1,5 +1,8 @@
 export { default as Topbar } from "./Topbar";
+export { default as Navbar } from "./Navbar";
 export { default as Layout } from "./Layout";
+export { default as ProjectCard } from "./ProjectCard";
+export { default as Form } from "./Form";
 export { default as ThemeSwitcher } from "./ThemeSwitcher";
 export { default as Dropdown } from "./Dropdown";
 export { default as Footer } from "./Footer";
