490891631744dd80b592f6a0db35533dedb85c61
Nabarun <nicknabarun@gmail.com>
2023-01-30 23:58:56 +0530

    Fix a bug

--- components/Form.jsx
+++ components/Form.jsx
@@ -34,7 +34,7 @@ const Form = () => {
   };
 
   const handleSubmit = async () => {
-    const regEx = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,8}(.[a-z{2,8}])?/g;
+    const regEx = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g;
 
     setError(false);
 
