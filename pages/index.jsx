66f1ec826e6c54ba2c2f462b5ac1ee4234913879
Nabarun <nicknabarun@gmail.com>
2023-01-31 17:21:08 +0530

    Fix a bug

--- pages/index.jsx
+++ pages/index.jsx
@@ -27,32 +27,29 @@ const Home = () => {
             />
 
             <div id='layout-child' className='md:max-w-4xl mt-5 md:mt-7'>
-              <h1 className='text-xl md:text-5xl lg:text-3xl xl:text-5xl font-bold cursor-default text-teal-700 dark:text-night-teal'>
+              <h1 className='text-xl md:text-5xl lg:text-5xl xl:text-5xl font-bold cursor-default text-teal-700 dark:text-night-teal'>
                 Software developer, web3 degen, and an amateur guitarist.
               </h1>
               <div className='md:text-justify font-semibold cursor-default mt-5 md:mt-9'>
                 <span className='flex'>
-                  <span className='text-base md:text-3xl lg:text-xl xl:text-3xl italic text-cyan-700 dark:text-purple-300'>
+                  <span className='text-base md:text-3xl lg:text-3xl xl:text-3xl italic text-cyan-700 dark:text-purple-300'>
                     Hi fren
                   </span>
                   <span className='self-center'>
                     <MdOutlineWavingHand className='h-4 w-4 md:h-7 md:w-7 ml-2 text-yellow-500 dark:text-yellow-300' />
                   </span>
                 </span>
-                <span className='flex text-base md:text-3xl lg:text-xl xl:text-3xl italic text-cyan-700 dark:text-purple-300 mb-5'>
+                <span className='flex text-base md:text-3xl lg:text-3xl xl:text-3xl italic text-cyan-700 dark:text-purple-300 mb-5'>
                   I am Nabarun
                 </span>
-                <p className='text-base md:text-xl lg:text-base xl:text-xl text-night-gray dark:text-gray-300 mb-5'>
+                <p className='text-base md:text-xl lg:text-xl xl:text-xl text-night-gray dark:text-gray-300 mb-5'>
                   A software developer and web3 autodidact based in India
                   currently. Along with taking keen interest in this space, I
                   truly believe that this is the next frontier and everyone
-                  should be comfortable with it. Programmable blockchain tech
-                  has already made huge dents out there and will continue doing
-                  so in all the industries we can possibly imagine rn. And it
-                  all starts with NFTs.
+                  should be comfortable with it.
                 </p>
                 <Link href='/email'>
-                  <button className='transition duration-300 transform bg-blue-500 dark:bg-night-blue hover:bg-blue-900 dark:hover:bg-indigo-700 inline-block text-base md:text-lg font-medium rounded-full text-white px-3 py-1 cursor-pointer'>
+                  <button className='transition duration-300 transform bg-blue-500 dark:bg-night-blue hover:bg-blue-900 dark:hover:bg-indigo-700 inline-block text-base md:text-xl font-medium rounded-full text-white px-3 py-1 cursor-pointer'>
                     Email Me!
                   </button>
                 </Link>
