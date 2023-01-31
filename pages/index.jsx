d116d9ab560b685ae5f54b5af6608f2015f27f01
Nabarun <nicknabarun@gmail.com>
2023-01-31 18:13:36 +0530

    Change button styles

--- pages/index.jsx
+++ pages/index.jsx
@@ -49,7 +49,7 @@ const Home = () => {
                   should be comfortable with it.
                 </p>
                 <Link href='/email'>
-                  <button className='transition duration-300 transform bg-blue-500 dark:bg-night-blue hover:bg-blue-900 dark:hover:bg-indigo-700 inline-block text-base md:text-xl lg:text-lg xl:text-xl font-medium rounded-full text-white px-3 py-1 cursor-pointer'>
+                  <button className='transition duration-300 transform bg-blue-500 dark:bg-night-blue hover:bg-blue-900 dark:hover:bg-indigo-700 inline-block text-base md:text-xl lg:text-lg xl:text-xl font-medium rounded-full text-white dark:text-night-white px-3 py-1 cursor-pointer'>
                     Email Me!
                   </button>
                 </Link>
