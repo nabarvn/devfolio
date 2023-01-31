0b8dd63c7bfbec7863240bda0d532053adfd31fa
Nabarun <nicknabarun@gmail.com>
2023-01-31 17:24:29 +0530

    Fix a bug

--- pages/index.jsx
+++ pages/index.jsx
@@ -22,7 +22,7 @@ const Home = () => {
               unoptimized
               height={100}
               width={100}
-              className='align-middle h-20 w-20 md:h-28 md:w-28 lg:h-24 lg:w-24 xl:h-28 xl:w-28 float-left rounded-full'
+              className='align-middle h-16 w-16 md:h-28 md:w-28 lg:h-24 lg:w-24 xl:h-28 xl:w-28 float-left rounded-full'
               src='/nabarun.png'
             />
 
@@ -32,24 +32,24 @@ const Home = () => {
               </h1>
               <div className='md:text-justify font-semibold cursor-default mt-5 md:mt-9'>
                 <span className='flex'>
-                  <span className='text-base md:text-3xl lg:text-3xl xl:text-3xl italic text-cyan-700 dark:text-purple-300'>
+                  <span className='text-base md:text-3xl lg:text-2xl xl:text-3xl italic text-cyan-700 dark:text-purple-300'>
                     Hi fren
                   </span>
                   <span className='self-center'>
                     <MdOutlineWavingHand className='h-4 w-4 md:h-7 md:w-7 ml-2 text-yellow-500 dark:text-yellow-300' />
                   </span>
                 </span>
-                <span className='flex text-base md:text-3xl lg:text-3xl xl:text-3xl italic text-cyan-700 dark:text-purple-300 mb-5'>
+                <span className='flex text-base md:text-3xl lg:text-2xl xl:text-3xl italic text-cyan-700 dark:text-purple-300 mb-5'>
                   I am Nabarun
                 </span>
-                <p className='text-base md:text-xl lg:text-xl xl:text-xl text-night-gray dark:text-gray-300 mb-5'>
+                <p className='text-base md:text-xl lg:text-lg xl:text-xl text-night-gray dark:text-gray-300 mb-5'>
                   A software developer and web3 autodidact based in India
                   currently. Along with taking keen interest in this space, I
                   truly believe that this is the next frontier and everyone
                   should be comfortable with it.
                 </p>
                 <Link href='/email'>
-                  <button className='transition duration-300 transform bg-blue-500 dark:bg-night-blue hover:bg-blue-900 dark:hover:bg-indigo-700 inline-block text-base md:text-xl font-medium rounded-full text-white px-3 py-1 cursor-pointer'>
+                  <button className='transition duration-300 transform bg-blue-500 dark:bg-night-blue hover:bg-blue-900 dark:hover:bg-indigo-700 inline-block text-base md:text-xl lg:text-lg xl:text-xl font-medium rounded-full text-white px-3 py-1 cursor-pointer'>
                     Email Me!
                   </button>
                 </Link>
