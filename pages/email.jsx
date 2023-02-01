63f87efed5820465e37e0fdae004bbaca899ab5f
Nabarun <nicknabarun@gmail.com>
2023-02-01 21:36:05 +0530

    Minor fix

--- pages/email.jsx
+++ pages/email.jsx
@@ -21,10 +21,9 @@ const Email = () => {
               </h1>
               <p className='text-base text-justify font-semibold cursor-default text-night-gray dark:text-gray-300 mb-9'>
                 Please feel free to message me, even if it&apos;s just to say
-                hi; I&apos;d appreciate it a ton. I&apos;m always open to new
-                opportunities and making awesome frens. Leave a message and I
-                shall get back to you ASAP. Thanks a bunch for stopping by. Talk
-                soon!
+                hi. I&apos;m always open to new opportunities and making awesome
+                frens. Leave a message and I shall get back to you ASAP. Thanks
+                a bunch for stopping by. Talk soon!
               </p>
               <div>
                 <Form />
