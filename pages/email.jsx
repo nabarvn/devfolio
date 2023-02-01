da3f23514805d8e35fa053cb095ae720983a6013
Nabarun <nicknabarun@gmail.com>
2023-02-01 21:32:51 +0530

    Alter content

--- pages/email.jsx
+++ pages/email.jsx
@@ -20,10 +20,11 @@ const Email = () => {
                 Get in Touch
               </h1>
               <p className='text-base text-justify font-semibold cursor-default text-night-gray dark:text-gray-300 mb-9'>
-                Thanks for stopping by. Please feel free to message me, even if
-                it&apos;s just to say hi; I&apos;d appreciate it a ton. I&apos;m
-                always open to new opportunities and making awesome frens! Leave
-                a message and I shall get back to you ASAP. <span>🤝</span>
+                Please feel free to message me, even if it&apos;s just to say
+                hi; I&apos;d appreciate it a ton. I&apos;m always open to new
+                opportunities and making awesome frens. Leave a message and I
+                shall get back to you ASAP. Thanks a bunch for stopping by. Talk
+                soon!
               </p>
               <div>
                 <Form />
