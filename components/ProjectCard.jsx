78e963c28074da04d67628cb2765930fd85faa3d
Nabarun <nicknabarun@gmail.com>
2023-02-22 15:58:04 +0530

    Fix a bug

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -5,11 +5,11 @@ const ProjectCard = ({ project }) => {
   return (
     <div className='flex flex-col bg-white dark:bg-night-gray shadow-lg transition duration-300 group dark:shadow-md hover:shadow-2xl dark:hover:shadow-night-teal rounded-lg p-5 pb-5 hover:cursor-pointer'>
       <Link href={project.projectSiteUrl} target='_blank'>
-        <h1 className='transition duration-300 mb-8 cursor-pointer group-hover:text-blue-500 dark:group-hover:text-night-teal text-xl md:text-3xl lg:text-4xl font-semibold'>
+        <h1 className='transition duration-300 mb-8 cursor-pointer group-hover:text-blue-500 dark:group-hover:text-night-teal text-xl lg:text-2xl font-semibold'>
           {project.title}
         </h1>
 
-        <p className='cursor-pointer text-justify text-base md:text-lg text-gray-700 dark:text-gray-300 font-normal mb-8'>
+        <p className='cursor-pointer text-justify text-base text-gray-700 dark:text-gray-300 font-normal mb-8'>
           {project.description}
         </p>
       </Link>
@@ -17,7 +17,7 @@ const ProjectCard = ({ project }) => {
         {project.tags.map((tag, i) => {
           return (
             <div key={i}>
-              <span className='inline-block ml-2 bg-blue-500 dark:bg-night-blue text-xs md:text-sm font-semibold rounded-full text-white px-2 py-1 mb-2 cursor-pointer'>
+              <span className='inline-block ml-2 bg-blue-500 dark:bg-night-blue text-xs font-semibold rounded-full text-white px-2 py-1 mb-2 cursor-pointer'>
                 {tag.name}
               </span>
             </div>
