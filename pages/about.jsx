a4234158836fd7ba966ece510cd4ef560fd0de6f
Nabarun <nicknabarun@gmail.com>
2023-03-05 02:26:06 +0530

    Alter content

--- pages/about.jsx
+++ pages/about.jsx
@@ -23,7 +23,7 @@ const About = () => {
               </h1>
               <div className='text-night-gray dark:text-gray-300 text-base md:text-lg font-semibold text-center mt-9'>
                 <p className='lg:hidden'>
-                  Howdy! My name is Nabarun (call me Nick if you&apos;d like)
+                  Howdy! My name is Nabarun (call me Nick if you&apos;d like),
                   and I&apos;m a Software Developer currently based in India. I
                   love being an autodidact as it enables my curious mind to
                   thrive. I&apos;m Dev #369 at Developer DAO and a member of
